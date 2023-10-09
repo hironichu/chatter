0118865aeabb3bb9787e517a1944697dddf7e466
Nassim Zen <hironichu@pm.me>
2023-10-09 11:48:54 +0200

    Added CI and supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://yxmurquzajrqxcpkavfb.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4bXVycXV6YWpycXhjcGthdmZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY4NDM3NjQsImV4cCI6MjAxMjQxOTc2NH0.Gg9-wAi1y_z-qSWPOwV_wjTUHoHfCrYspr54H67Q_e0',
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
